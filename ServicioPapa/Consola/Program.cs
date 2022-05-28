7a9c05c8bd63a9f077c8d999397927b250c047d2
Pablo_Viano <pabloviano1@gmail.com>
2022-05-28 01:57:57 -0300

    Falta Mucho

---
+++ ServicioPapa/Consola/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.ServiceClient service = new ServiceReference1.ServiceClient();
+            Console.WriteLine("Comienza el juego");
+            bool Estado = true;
+            while (Estado)
+            {
+                int puntos = service.VerificarPuntos(ObtenerNumeros(5));
+            }
+            
+        }
+        private List<int> ObtenerNumeros(int dados)
+        {
+            List<int> Numeros = new List<int>();
+            for (int i = 0; i <= dados; i++)
+            {
+                Random n1 = new Random();
+                n1.Next(1, 6);
+                Numeros.Add(int.Parse(n1.ToString()));
+            }
+            return Numeros;
+        }
+    }
+}
---
+++ ServicioPapa/ServicioPapa/App_Code/Service.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+	public int Dados = 5;
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+	public int VerificarPuntos(List<int> numeros)
+    {
+		int Puntos = 0;
+        if (numeros.Distinct().Count() == 5)
+        {
+			Puntos += -100;
+        }
+        else
+        {
+			if (VerificarEscalera(numeros))
+            {
+				Puntos += 20;
+            }
+            else
+            {
+				if (Coincidencias(numeros) != 0)
+				{
+					Puntos += Coincidencias(numeros);
+				}
+			}
+        }
+		return Puntos;
+    }
+	private bool VerificarEscalera(List<int> numeros)
+    {
+		bool Escalera = true;
+		numeros = numeros.OrderBy(x => x).ToList();
+		int cont = 0;
+		while (cont < 4)
+        {
+			if (numeros[cont] + 1 != numeros[cont + 1])
+            {
+				Escalera = false;
+            }
+			cont++;
+        }
+		return Escalera;
+    }
+	private int Coincidencias(List<int> numeros)
+	{
+		int Puntos = 0;
+		foreach (int item in numeros)
+		{
+			int repeticiones = numeros.FindAll(x => x == item).Count;
+			if (repeticiones >= 1)
+			{
+				Puntos += item * repeticiones;
+			}
+			this.Dados -= repeticiones;
+		}
+		return Puntos;
+	}
+}
